037e102b2861468ce84ec7305911a44c512d8f18
mihaiturcu <contact@mihaiturcu.ro>
2016-05-28 19:31:32 +0300

    found out how to debug in vs with arguments =] yay

--- Order Matrix Lines/Program.cs
+++ Order Matrix Lines/Program.cs
@@ -13,24 +13,40 @@ namespace Order_Matrix_Lines
         {
             List<int> line = null;
             List<List<int>> matrix = null;
-            String elem;
+            String buffer;
             using (StreamReader sr = new StreamReader(arg))
             {
-                while ((elem = sr.ReadLine()) != null)
+                while ((buffer = sr.ReadLine()) != null)
                 {
-                    Console.WriteLine(elem);
+                    string[] elements = buffer.Split(',');
+                    foreach(string elem in elements)
+                    {
+                        int tempCastvalue;
+                        int.TryParse(elem,out tempCastvalue);
+                        line.Add(tempCastvalue);
+                    }
+                    matrix.Add(line);
                 }
 
             }
-
             return matrix;
 
         }
 
+        public static void nice_print(List<List<int>> matrix)
+        {
+            foreach(List<int> list in matrix)
+            {
+                foreach(int elem in list)
+                {
+                    Console.Write(elem);
+                }
+                Console.Write("\n");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
-            Console.WriteLine(args);
             read_matrix(args[0].ToString());
             
         }
