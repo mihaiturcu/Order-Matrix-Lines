8ccbf7a0b683b26cb7762d513b8ce5637a7439d1
mihaiturcu <contact@mihaiturcu.ro>
2016-05-28 18:11:51 +0300

    no idea what i'm doing

--- Order Matrix Lines/Program.cs
+++ Order Matrix Lines/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,29 @@ namespace Order_Matrix_Lines
 {
     class Program
     {
+        public List<List<int>> read_matrix(string arg)
+        {
+            List<int> line = null;
+            List<List<int>> matrix = null;
+            String elem;
+            using (StreamReader sr = new StreamReader(arg))
+            {
+                while ((elem = sr.ReadLine().Trim(',')) != null)
+                {
+                    Console.WriteLine(elem);
+                }
+
+            }
+
+            return matrix;
+
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("test");
+            
+            
         }
     }
 }
